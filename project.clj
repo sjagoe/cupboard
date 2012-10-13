00d5fdbb2c33111157eaab071104d254324d6d9f
Simon Jagoe <sjagoe@enthought.com>
2012-10-13 18:11:27 +0100

    Move to personal namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cupboard "1.0.0-SNAPSHOT"
+(defproject org.clojars.simonjagoe/cupboard "1.0.0-SNAPSHOT"
   :description "An embedded database library for Clojure."
   :repositories {"oracle" "http://download.oracle.com/maven"}
   :dependencies [[org.clojure/clojure "1.4.0"]
